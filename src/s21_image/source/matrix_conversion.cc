07d1e42b0695eab7d831672dda17ed7b203cdf2a
Leontine Alex <lalex@uwd.com>
2023-05-13 20:00:02 +0300

    Fixed image rescaling

--- src/s21_image/source/matrix_conversion.cc
+++ src/s21_image/source/matrix_conversion.cc
@@ -3,10 +3,23 @@
 
 
 s21::Matrix s21::Image::ToVector() {
+
   auto matrix = Matrix(1, width_ * height_);
-  auto max_color = (1 << bits_per_pixel_) - 1;
+
+  uint max_color = 0;
+  for (std::size_t x = 0; x < width_; x++) {
+
+    for (std::size_t y = 0; y < height_; y++) {
+
+      if (GetPixel(x, y) > max_color)
+        max_color = GetPixel(x, y);
+    }
+  }
+
   auto data = matrix.data();
+
   for (std::size_t i = 0; i < width_ * height_; i++) {
+
     auto y = i / height_;
     auto x = i % height_;
     data[i] = 1.0 - 1.0 * GetPixel(y, x) / max_color;
